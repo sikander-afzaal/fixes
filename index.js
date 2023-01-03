8239c902f44a351a2ff47f08f8b49b461182a253
sikander-afzaal <sikander.aaa@gmail.com>
2023-01-03 13:25:11 +0500

    first

---
+++ index.js
@@ -0,0 +1,46 @@
+let acc = document.getElementsByClassName("main-faq");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("h-max");
+
+    let btn = this.firstElementChild.firstElementChild.firstElementChild;
+
+    let faq_content = this.nextElementSibling;
+    if (faq_content.style.maxHeight) {
+      faq_content.style.maxHeight = null;
+      btn.innerHTML = "+";
+      btn.style.background = "transparent";
+      btn.style.color = "#9693b8";
+    } else {
+      btn.innerHTML = "-";
+      btn.style.background = "rgb(93, 79, 255)";
+      btn.style.color = "white";
+      faq_content.style.maxHeight = faq_content.scrollHeight + "px";
+    }
+  });
+}
+
+document.getElementById("default").click();
+
+function showTabData(evt, tabname) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("w-84");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.querySelectorAll("ul li");
+
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("text-[#ff5c35]", "");
+  }
+  evt.currentTarget.className += "text-[#ff5c35]";
+  if (tabname == "All") {
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "block";
+    }
+    return;
+  }
+  document.getElementById(tabname).style.display = "block";
+}
